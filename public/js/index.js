d5ff55325dca53e55f99632690da0136778699cf
gyGil <geunyoungGil@gmail.com>
2018-08-04 22:03:26 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected on server');
-
-    socket.emit('createMessage', {
-        from: 'Gil',
-        text: 'Hey, this is Gil'
-    });
 });
 
 socket.on('disconnect', function() {
@@ -15,4 +10,18 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('New Message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
